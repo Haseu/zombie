763e3dcb0f6a52aeb989eeb367f8eaa853c02548
André Menutole <andremenutole@gmail.com>
2020-02-16 02:49:19 -0300

    Adiconado animação, e controle de movimentos do zumbi

---
+++ Assets/Scripts/ControlaZumbi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaZumbi : MonoBehaviour
+{
+    public int speed = 5; 
+    public GameObject jogador;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector3 direcao = jogador.transform.position - transform.position;
+
+         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao.normalized * speed * Time.deltaTime));
+    }
+}
