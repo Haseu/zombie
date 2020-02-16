bda19f07b276b0212ee1438121cd5cdfcf2f851e
André Menutole <andremenutole@gmail.com>
2020-02-16 01:38:42 -0300

    Camera configurada para seguir o jogador

---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour
+{
+    public GameObject jogador;
+    Vector3 distanciaCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaCamera = transform.position - jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jogador.transform.position + distanciaCamera;
+    }
+}
