795831a56303292c37b54e5993777d9c61ebdbed
André Menutole <andremenutole@gmail.com>
2020-02-20 22:59:26 -0300

    Adicionado spawn aleatorio de chefe (posicao)

--- Assets/Scripts/GeradorChefe.cs
+++ Assets/Scripts/GeradorChefe.cs
@@ -8,18 +8,39 @@ public class GeradorChefe : MonoBehaviour
     public float tempoEntreGeracao = 60;
     public GameObject chefe;
     private ControlaInterface controlaInterface;
+    public Transform[] posicoesPossiveisGeracao;
+    private Transform jogador;
 
     private void Start() {
         tempoGeracao = tempoEntreGeracao;
         controlaInterface = GameObject.FindObjectOfType(typeof(ControlaInterface)) as ControlaInterface;
+        jogador = GameObject.FindWithTag("Jogador").transform;
     }
 
     private void Update() {
         if (Time.timeSinceLevelLoad > tempoEntreGeracao)
         {
-            Instantiate(chefe, transform.position, Quaternion.identity);
+            Vector3 posicaoCriacao = this.calcularPosicaoMaisDistanteJogador();
+            Instantiate(chefe, posicaoCriacao, Quaternion.identity);
             controlaInterface.mostraTextoChefe();
             tempoEntreGeracao = Time.timeSinceLevelLoad + tempoEntreGeracao;
         }
+    }
+
+    private Vector3 calcularPosicaoMaisDistanteJogador()
+    {
+        Vector3 posicaoDeMaiorDistancia = Vector3.zero;
+        float maiorDistancia = 0;
+        foreach (Transform posicao in posicoesPossiveisGeracao)
+        {
+            float distanciaEntreJogador = Vector3.Distance(posicao.position, jogador.position);
+            if(distanciaEntreJogador > maiorDistancia)
+            {
+                maiorDistancia = distanciaEntreJogador;
+                posicaoDeMaiorDistancia = posicao.position;
+            }
+        }
+
+        return posicaoDeMaiorDistancia;
     }
 }
